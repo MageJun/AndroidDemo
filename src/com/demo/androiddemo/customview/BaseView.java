6e969558f71ae40c2016aa89721620217ac96c62
MageJun <Lei__1991@163.com>
2017-03-08 13:39:20 +0800

    新增两个视图类

---
+++ src/com/demo/androiddemo/customview/BaseView.java
@@ -0,0 +1,12 @@
+package com.demo.androiddemo.customview;
+
+import android.content.Context;
+import android.view.View;
+
+public abstract class BaseView extends View {
+
+	public BaseView(Context context) {
+		super(context);
+	}
+
+}

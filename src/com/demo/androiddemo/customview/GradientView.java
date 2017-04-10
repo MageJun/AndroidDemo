501a9b77772232f6be460619901604e2cd260b60
MageJun <Lei__1991@163.com>
2017-04-10 23:53:18 +0800

    Canvas.rotate测试

--- src/com/demo/androiddemo/customview/GradientView.java
+++ src/com/demo/androiddemo/customview/GradientView.java
@@ -37,7 +37,7 @@ public class GradientView extends BaseView {
 		mDisplay = Utils.getDisplayMetrics(context);
 		mPaint = new Paint();
 		mPaint.setAntiAlias(true);
-		mPaint.setStyle(Style.STROKE);
+		mPaint.setStyle(Style.FILL);
 		mPaint.setStrokeWidth(5);
 		mPaint.setColor(Color.RED);
 		
@@ -53,14 +53,27 @@ public class GradientView extends BaseView {
 		Rect r = new Rect(width/2-100, height/2-100,width/2+100, height/2+100);
 		canvas.save();
 		canvas.clipRect(r);
+		Rect innerR = new Rect(r.left+30, r.top+30, r.right-30, r.bottom-30);
+		canvas.drawColor(Color.YELLOW);
 //		canvas.rotate(45,width/2,height/2);
-		mGradient.setBounds(r);
-//		mGradient.setBounds(width/2-100, height/2-100,width/2+100, height/2+100);
-		mGradient.setColors(new int[]{0x333333, 0xB0333333});
-		mGradient.setOrientation(Orientation.BL_TR);
-		mGradient.draw(canvas);
+//		canvas.drawRect(innerR, mPaint);
 		
+//		float  angle = (float) Math.toDegrees(Math.atan2(Math.abs(r.top-r.bottom),(Math.abs(r.right-r.left))));
+		float  angle = (float) Math.toDegrees(Math.atan2((r.top-r.bottom),(r.right-r.left)));
+		canvas.rotate(angle,r.left,r.bottom);
+//		Rect bounds = new Rect(r.left+50-25, r.left-50, r.right+50, r.left);
+		int bottom = (int) Math.sqrt(r.left*r.left+r.bottom*r.bottom);
+		Rect bounds = new Rect(0, bottom-25, 50, bottom);
+		
+//		mGradient.setBounds(r);
+////		mGradient.setBounds(width/2-100, height/2-100,width/2+100, height/2+100);
+//		mGradient.setColors(new int[]{0x333333, 0xB0333333});
+//		mGradient.setBounds(bounds);
+//		mGradient.setOrientation(Orientation.BL_TR);
+//		mGradient.draw(canvas);
+		canvas.drawRect(bounds, mPaint);
 		canvas.restore();
+		
 //		canvas.restore();
 	}
 	@Override
